af7ef18fa9b7e192c15066b5c1abb621ead2de22
Shahrukh Karim <Bcsbs2212236@szabist.pk>
2024-05-29 14:08:00 +0500

    Add files via upload

---
+++ Q1/QuickSort/src/QuickSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] data = {2, 8, 7, 1, 3, 5, 6, 4};
+        quickSort(data, 0, data.length - 1);
+        System.out.println("Sorted data: " + Arrays.toString(data));
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+}
